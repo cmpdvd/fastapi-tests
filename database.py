35437da48adab943da6820649643e4562cafc02f
david campion <david@iconosquare.com>
2025-08-14 12:57:13 +0200

    updated DATABASE_URL web

--- database.py
+++ database.py
@@ -3,7 +3,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://userdc:passworddc@localhost:5432/dbdc"
+# DATABASE_URL = "postgresql://userdc:passworddc@localhost:5432/dbdc"
+DATABASE_URL = "postgresql://postgres:splgiJFbXLyOwuWwQeascrFjFDheoJtV@postgres.railway.internal:5432/railway"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
