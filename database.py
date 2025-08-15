635dd0bd1edc7eefcff68384b38f1f2806f5980d
david campion <david@iconosquare.com>
2025-08-15 09:34:10 +0200

    fix alembic.ini

--- database.py
+++ database.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 # DATABASE_URL = "postgresql://userdc:passworddc@localhost:5432/dbdc"
 DATABASE_URL = "postgresql://userdc:passworddc@postgres.railway.internal:5432/dbdc"
+# DATABASE_URL = "postgresql://userdc:passworddc@postgres.railway.internal:5432/railway"
 # DATABASE_URL = "postgresql://postgres:splgiJFbXLyOwuWwQeascrFjFDheoJtV@postgres.railway.internal:5432/railway"
 
 engine = create_engine(DATABASE_URL)
