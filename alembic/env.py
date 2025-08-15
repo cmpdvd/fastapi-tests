78d6651fdea1e735377ef4edcc8074661b5ec898
david campion <david@iconosquare.com>
2025-08-15 12:05:51 +0200

    fix env.py

--- alembic/env.py
+++ alembic/env.py
@@ -19,6 +19,7 @@ from alembic import context
 # access to the values within the .ini file in use.
 config = context.config
 
+
 # config.set_main_option("sqlalchemy.url", os.getenv("DATABASE_URL"))
 
 # Interpret the config file for Python logging.
@@ -27,6 +28,9 @@ if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
 print("DEBUG DATABASE_URL =", os.getenv("DATABASE_URL")) # check if DATABASE_URL is set
+
+# ------------------------------------------------------------
+database_url = os.getenv('DATABASE_URL')
 
 
 
@@ -54,7 +58,8 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    # url = config.get_main_option("sqlalchemy.url")
+    url = database_url  # use DATABASE_URL from env
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -73,11 +78,21 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
+
+    configuration = config.get_section(config.config_ini_section)
+    configuration['sqlalchemy.url'] = database_url  # Ajoutez cette ligne
+    
     connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
+        configuration,
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
+
+    # connectable = engine_from_config(
+    #     config.get_section(config.config_ini_section, {}),
+    #     prefix="sqlalchemy.",
+    #     poolclass=pool.NullPool,
+    # )
 
     with connectable.connect() as connection:
         context.configure(
