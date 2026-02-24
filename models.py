c6ad841034db523286e5cb0d4a972002d2b481f4
cmpdvd <cmpdvd@gmail.com>
2026-02-24 23:08:01 +0100

    added: created_at

--- models.py
+++ models.py
@@ -1,9 +1,11 @@
-from sqlalchemy import Column, Integer, String
+import uuid
+from datetime import time
 from database import Base
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.types import String
 from sqlalchemy.types import UUID
-import uuid
+from sqlalchemy.dialects.postgresql import TIMESTAMP
+from sqlalchemy import Column, Integer, String, DateTime, func
 
 
 class User(Base):
@@ -11,9 +13,19 @@ class User(Base):
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     display_name = Column(String, index=True)
     email = Column(String, unique=True, index=True)
+    created_at = Column(
+        TIMESTAMP(timezone=True),
+        server_default=func.now(),
+        nullable=False,
+    )
 class Quote(Base):
     __tablename__ = "anecdotes"
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     quote = Column(String, index=True)
     child_name = Column(String, index=True)
+    created_at = Column(
+        TIMESTAMP(timezone=True),
+        server_default=func.now(),
+        nullable=False,
+    )
 
