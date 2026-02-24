0637d1ab708042a308c0a405a59c0829988b48d0
cmpdvd <cmpdvd@gmail.com>
2026-02-24 20:09:18 +0100

    change model

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
+    display_name = Column(String, index=True)
     email = Column(String, unique=True, index=True)
 class Customer(Base):
     __tablename__ = "customers"
