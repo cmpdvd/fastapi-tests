0ac8d8f6e5858de2e5a41764da6d88feda34707f
cmpdvd <cmpdvd@gmail.com>
2026-02-28 15:03:07 +0100

    fix: move git root to project root

---
+++ app/fastApi/start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo "Running Alembic migrations..."
+python -m alembic upgrade head
+
+# Display the environment variables for debug
+echo "📊 Variables of the database:"
+env | grep -i -E "(database|postgres|pg|db)" | head -10
+
+
+echo "Starting web server..."
+python -m uvicorn app.fastApi.main:app --host 0.0.0.0 --port 8000
+
+
+
+# # Run the migrations
+# echo "🔄 Running migrations..."
+# python run_migrations.py
+
+# # Start the web server
+# echo "🌐 Starting the web server..."
+# exec uvicorn main:app --host 0.0.0.0 --port $PORT
+
